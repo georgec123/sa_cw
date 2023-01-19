32f076a33b45c79e0d9140c2ab4f59439f98b859
George Coxon <george.coxon@live.co.uk>
2023-01-19 18:50:53 +0000

    new euro data

--- data/get_data.py
+++ data/get_data.py
@@ -1,6 +1,6 @@
 import os
 from cryptocmd import CmcScraper
-
+import yfinance
 
 prices_dir = os.path.join(os.path.dirname(__file__), 'prices')
 
@@ -14,3 +14,13 @@ for coin in coins:
     file_path = os.path.join(prices_dir, f'./{coin}.csv')
     df.to_csv(file_path, index=False)
     print(f"Done {coin}")
+
+
+# get eur data
+euro = yfinance.Ticker('EURUSD=X').history(
+    start='2014-01-01', end='2023-01-10')
+euro.reset_index(inplace=True)
+euro = euro.rename(columns={'Close': 'Price'})
+
+euro.to_csv(os.path.join(prices_dir, 'EURUSD.csv'),
+            index=False, header=True)
